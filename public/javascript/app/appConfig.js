66d1bbb9b14783babe551d800a0b438214513632
Nikita Klyushnikov <vipkonst@gmail.com>
2014-06-14 17:07:59 +0300

    Updated angular app structure, added required controllers

--- public/javascript/app/appConfig.js
+++ public/javascript/app/appConfig.js
@@ -2,16 +2,42 @@
 
 (function (window, undefined) {
     angular.module('itechart-connect')
-        .config(['$stateProvider', '$urlRouterProvider',
-            function($stateProvider, $urlRouterProvider) {
+        .config(['$stateProvider', '$urlRouterProvider', '$locationProvider',
+            function($stateProvider, $urlRouterProvider, $locationProvider) {
                 //default route
                 $urlRouterProvider.otherwise("/");
 
                 $stateProvider
-                    .state('homePageState', {
+                    .state('company', {
+                        url: '/company/',
+                        templateUrl: '/javascript/app/companyPage/views/companyPage.html',
+                        controller: 'companyPageCtrl'
+                    })
+                    .state('departments',    {
+                        url: '/departments/',
+                        templateUrl: '/javascript/app/departmentsPage/views/departmentsPage.html',
+                        controller: 'departmentsPageCtrl'
+                    })
+                    .state('groups', {
+                        url: '/groups/',
+                        templateUrl: '/javascript/app/groupsPage/views/groupsPage.html',
+                        controllers: 'groupsPageCtrl'
+                    })
+                    .state('home', {
                         url: '/',
-                        templateUrl: 'javascript/app/homePage/views/homePage.html',
+                        templateUrl: '/javascript/app/homePage/views/homePage.html',
                         controller: 'homePageCtrl'
+                    })
+                    .state('newcomers', {
+                        url: '/newcomers/',
+                        templateUrl: '/javascript/app/newcomersPage/views/newcomersPage.html',
+                        controller: 'newcomersPageCtrl'
+                    })
+                    .state('rooms', {
+                        url: '/rooms/',
+                        templateUrl: '/javascript/app/roomsPage/views/roomsPage.html'
                     });
+
+                //$locationProvider.html5Mode(true);
         }]);
 })(window);
