6bd5e11eb67e4c0cc4571edc2dbb448421e45660
Nikita Klyushnikov <vipkonst@gmail.com>
2014-06-14 21:14:38 +0300

    added side bar tile directive

--- public/javascript/app/homePage/controllers/homePageCtrl.js
+++ public/javascript/app/homePage/controllers/homePageCtrl.js
@@ -2,6 +2,38 @@
 
 (function (window, undefined) {
     angular.module('itechart-connect')
-        .controller('homePageCtrl', ['$scope', function($scope) {
-        }]);
+        .controller('homePageCtrl', function($scope) {
+            $scope.sidebarTiles = [
+                {
+                    iconClass: 'fa-child',
+                    title: 'People',
+                    backgroundImgUrl: '/images/tiles/people.jpg',
+                    destinationUrl: '#'
+                },
+                {
+                    iconClass: 'fa-files-o',
+                    title: 'Docs',
+                    backgroundImgUrl: '/images/tiles/docs.jpg',
+                    destinationUrl: '#'
+                },
+                {
+                    iconClass: 'fa-camera-retro',
+                    title: 'Photos',
+                    backgroundImgUrl: '/images/tiles/photos.jpg',
+                    destinationUrl: '#'
+                },
+                {
+                    iconClass: 'fa-glass',
+                    title: 'Events',
+                    backgroundImgUrl: '/images/tiles/events.jpg',
+                    destinationUrl: '#'
+                },
+                {
+                    iconClass: 'fa-calendar',
+                    title: 'Meetups',
+                    //backgroundImgUrl: '/images/tiles/meetups.png',
+                    destinationUrl: '#'
+                }
+            ];
+        });
 })(window);
