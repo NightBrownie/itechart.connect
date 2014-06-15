46fe6d538976e15c027a75d1b0fe04d75a1273aa
Nikita Klyushnikov <vipkonst@gmail.com>
2014-06-15 10:52:46 +0300

    fix profile page

--- public/javascript/app/profilePage/controllers/profilePageCtrl.js
+++ public/javascript/app/profilePage/controllers/profilePageCtrl.js
@@ -25,7 +25,7 @@
                             }
                         }
 
-                        if (smgUser == undefined) {
+                        if ($scope.smgUser == undefined) {
                             $location.url('/');
                             return;
                         }
