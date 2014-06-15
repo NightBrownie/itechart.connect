970b308acb756e998103c9fb9621bb3a1c80a8bc
Alexey Dudarev <dudarev.av@gmail.com>
2014-06-15 11:24:02 +0200

    events

--- modules/feedItems/router.js
+++ modules/feedItems/router.js
@@ -5,9 +5,34 @@ var router = require('express').Router(),
 
 router.route('/getFeed')
     .post(function (req, res) {
-        controller.getFeedItems(req.body, function(err, items) {
-            return res.send(items);
-        });
+        /*controller.getFeedItems(req.body, function(err, items) {
+         return res.send(items);
+         });*/
+        setTimeout(
+            function () {
+                return res.send([
+                    {
+                        title: 'Наперегонки с Леманом - Хакатон в Itechart',
+                        shortContent: 'Кто же победит',
+                        type: '1',
+                        visibility: '0'
+                    },
+                    {
+                        title: 'Ночь пережили не все',
+                        shortContent: 'Кто же пришел чисто поесть?!',
+                        type: '1',
+                        visibility: '0'
+                    },
+                    {
+                        title: 'Прошли семинары по актуальным темам',
+                        shortContent: 'Смотрите на DVD',
+                        type: '1',
+                        visibility: '0'
+                    }
+                ]);
+            },
+            200
+        );
     });
 
 module.exports = router;
